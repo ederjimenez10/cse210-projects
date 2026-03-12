5fecc8cde2b813dc008320eba44b41969d123b48
ederjimenez10 <ederjimenez10@gmail.com>
2026-03-11 22:15:19 -0600

    Worked on the exercise 2 of week 1 for a grade letter calculator.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Grade Letter Calculator.");
+        Console.Write("Enter your grade percentage: ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            // Console.WriteLine("A");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            // Console.WriteLine("B");
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            // Console.WriteLine("C");
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            // Console.WriteLine("D");
+            letter = "D";
+        }
+        else
+        {
+            // Console.WriteLine("F");
+            letter = "F";
+        }    
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the course. Congratulations!");
+        }    
+        else
+        {
+            Console.WriteLine("Try harder next time.");
+        }
+        Console.WriteLine($"Your grade letter is {letter}.");
     }
 }
