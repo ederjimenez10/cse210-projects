164274df73f2b0dba7207ca731d2fb8b02539e7d
ederjimenez10 <ederjimenez10@gmail.com>
2026-03-14 21:29:06 -0600

    Worked on the Number Guesser game program of week 1.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        
+
+        // Console.Write("What is the magic number? ");
+        // string ask = Console.ReadLine();
+        // int magicNumber = int.Parse(ask);
+        Random randomGenerator = new();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            // string guess = Console.ReadLine();
+            // guessNumber = int.Parse(guess);
+            guessNumber = int.Parse(Console.ReadLine()); //<- Simpler way to do it.
+
+
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guessNumber != magicNumber);
     }
 }
